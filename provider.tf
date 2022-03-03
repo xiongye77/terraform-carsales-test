15544b27cb426af1c37555e741844d9e9063967c
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-03-03 11:07:24 +1100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,11 +4,13 @@ provider "aws" {
 }
 
 # your normal provider
-provider "aws" {
-  version = ">= 3.4.0"
-}
-
+#provider "aws" {
+#  version = ">= 3.4.0"
+#}
+# version 4 has some changes https://stackoverflow.com/questions/71078462/terraform-aws-provider-error-value-for-unconfigurable-attribute-cant-configur
 terraform {
   required_version = ">= 0.13.1"
+  required_providers {
+    aws  = "~> 3.73.0"
+  }
 }
-
