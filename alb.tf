10b3c1bd68e21f61389a6422ecd3f2b743a3c805
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-03-10 20:42:28 +1100

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -51,12 +51,12 @@ resource "aws_lb" "carsales_alb" {
 
 resource "aws_lb_listener" "carsales_http" {
   load_balancer_arn = aws_lb.carsales_alb.arn
-  #port = 443
-  port = 80
-  protocol = "HTTP"
-  #protocol = "HTTPS"
-  #ssl_policy        = "ELBSecurityPolicy-TLS-1-0-2015-04"
-  #certificate_arn   = aws_acm_certificate.myapp.arn
+  port = 443
+  #port = 80
+  #protocol = "HTTP"
+  protocol = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.myapp_alb.arn
   #default_action {
   #type = "forward"
   #target_group_arn = aws_lb_target_group.carsales-back-end-tg-1.arn
@@ -241,3 +241,33 @@ resource "aws_lb_target_group_attachment" "lambda_attachment" {
   target_id = aws_lambda_function.test_lambda.arn
   depends_on = [ aws_lambda_permission. with_lb]
 }
+
+
+
+
+resource "aws_acm_certificate" "myapp_alb" {
+  #provider          = "aws.acm_provider"
+  domain_name       = "${var.demo_dns_name}.${data.aws_route53_zone.public.name}"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# This is a DNS record for the ACM certificate validation to prove we own the domain
+#
+# This example, we make an assumption that the certificate is for a single domain name so can just use the first value of the
+# domain_validation_options.  It allows the terraform to apply without having to be targeted.
+# This is somewhat less complex than the example at https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
+# - that above example, won't apply without targeting
+
+resource "aws_route53_record" "cert_validation_alb" {
+  allow_overwrite = true
+  name            = tolist(aws_acm_certificate.myapp_alb.domain_validation_options)[0].resource_record_name
+  records         = [ tolist(aws_acm_certificate.myapp_alb.domain_validation_options)[0].resource_record_value ]
+  type            = tolist(aws_acm_certificate.myapp_alb.domain_validation_options)[0].resource_record_type
+  zone_id  = data.aws_route53_zone.public.id
+  ttl      = 60
+  provider = aws.account_route53
+}
+
