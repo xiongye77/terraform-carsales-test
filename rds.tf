0ac6a41230cb154af38332ef75943b6b6159d014
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-03-12 16:33:12 +1100

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -38,24 +38,31 @@ resource "aws_db_subnet_group" "carsales-db-subnet" {
   }
 }
 
-# Create CarSales Database Instance 
+# Create CarSales Database Instance
 
 resource "aws_db_instance" "carsales-db" {
-  allocated_storage       = "10"
+  allocated_storage       = "20"
   storage_type            = "gp2"
+  #storage_type            = "io1"   io1 size at least 100G
+  #iops = "3000"
   engine                  = "postgres"
-  engine_version          = "11.12"
+  engine_version          = "12.9"
   multi_az                = "true"
-  instance_class          = "db.t2.micro"
-  name                    = "magento"
+  terraform-carsales-test = "30" # interval of Enhanced Monitoring metrics are collected for the DB instance
+  instance_class          = "db.t3.large"
+  name                    = "carsalesdb"
   # Set the secrets from AWS Secrets Manager
-  username = "admin12345"
-  password = "${random_string.password.result}"  
-  identifier              = "carsales-database"
+  username = var.rds_username
+  password = "${random_string.password.result}"
+  identifier              = "carsales-db"
   skip_final_snapshot     = "true"
-  backup_retention_period = "7"
+  publicly_accessible    = "false"
+  monitoring_interval = "30"   # interval for collecting Enhanced Monitoring metrics
+  performance_insights_enabled = true
+  performance_insights_retention_period = 7
+  backup_retention_period = "1"
   # DB Instance class db.t2.micro does not support encryption at rest
-  storage_encrypted       = "false"
+  storage_encrypted       = "true"
   db_subnet_group_name    = aws_db_subnet_group.carsales-db-subnet.name
   vpc_security_group_ids  = [aws_security_group.carsales_db_sg.id]
    tags = {
@@ -63,6 +70,29 @@ resource "aws_db_instance" "carsales-db" {
     Terraform   = "true"
   }
 }
+
+
+
+resource "aws_db_instance" "carsales-db-replica" {
+  identifier             = "carsales-db-replica"
+  replicate_source_db    = aws_db_instance.carsales-db.identifier ## refer to the master instance
+  name                   = "carsalesdbreplica"
+  instance_class         = "db.t3.large"
+  allocated_storage      = "20"
+  storage_type            = "gp2"
+  #storage_type            = "io1" io1 size at least 100G
+  #iops = "3000"
+  engine                 = "postgres"
+  engine_version         = "12.9"
+  skip_final_snapshot    = "true"
+  publicly_accessible    = "false"
+  vpc_security_group_ids = [aws_security_group.carsales_db_sg.id]
+# Username and password must not be set for replicas
+  storage_encrypted       = "true"
+# disable backups to create DB faster
+  backup_retention_period = 0
+}
+
 
 resource "random_string" "password" {
   length  = 16
@@ -85,8 +115,22 @@ locals {
     data.aws_secretsmanager_secret_version.creds.secret_string
   )
 }
+
+resource "aws_ssm_parameter" "postgres_username" {
+  name  = "/production/myapp/rds-postgres-username"
+  type  = "String"
+  value = var.rds_username
+}
+
 resource "aws_ssm_parameter" "db_host" {
   name  = "/production/myapp/db-host"
   type  = "String"
   value = aws_db_instance.carsales-db.endpoint
+}
+
+
+resource "aws_ssm_parameter" "db_host_read_replica" {
+  name  = "/production/myapp/db-host-read-replica"
+  type  = "String"
+  value = aws_db_instance.carsales-db-replica.endpoint
 }
