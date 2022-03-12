1f0ec1f01555b992d6aa04fd8f6e4ec30cc49b36
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-03-12 16:35:13 +1100

    Update ssm.tf

--- ssm.tf
+++ ssm.tf
@@ -4,3 +4,11 @@ resource "aws_ssm_parameter" "cloudwatch-linux-mem" {
   value = file("ssm.json")
 }
 
+resource "aws_ssm_parameter" "ASG_launch_template_version" {
+  name  = "ASG_launch_template_version"
+  type  = "String"
+  value = "${aws_launch_template.ecs.latest_version}"
+  lifecycle {
+    ignore_changes = all
+  }
+}
